39572fc9ce1cecd6daaeecfada37013d9460fc84
Nathan Giles <gills11@gmail.com>
2021-07-19 23:16:49 +0100

    Operational Program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
         Lead lead1 = new Lead("Test1", "testnum1", "test1@test.gmail.com", "TestCompany1");
-        Lead lead2 = new Lead("Test2", "testnum1", "test1@test.gmail.com", "TestCompany1");
-        Lead lead3 = new Lead("Test3", "testnum1", "test1@test.gmail.com", "TestCompany1");
+        Lead lead2 = new Lead("Test2", "testnum2", "test2@test.gmail.com", "TestCompany2");
+        Lead lead3 = new Lead("Test3", "testnum2", "test2@test.gmail.com", "TestCompany2");
 
         MainMenu.theLeads.put(lead1.getId(), lead1);
         MainMenu.theLeads.put(lead2.getId(), lead2);
@@ -18,7 +18,6 @@ public class Main {
                 menu.OS();
             }
         } catch (RuntimeException e) {
-
         }
     }
 }
